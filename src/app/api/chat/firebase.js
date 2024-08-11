9bb883eb1a63ee505b2b701fb30421d5d96f9456
DanTiw <danishtiwari2204@gmail.com>
2024-08-11 23:26:13 +0530

    Finalized Changes

--- src/app/api/chat/firebase.js
+++ src/app/api/chat/firebase.js
@@ -1,5 +1,5 @@
 // Import the functions you need from the SDKs you need
-
+"use client"
 import firebase from 'firebase/app';
 import { initializeApp } from "firebase/app";
 import { getFirestore } from "firebase/firestore";
